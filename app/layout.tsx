40c24cafa35980d93cd2f72a845490b174d1e611
Steven Quintana <stiven0721@hotmail.com>
2023-04-04 17:21:55 -0500

    feat: connect to mongoDB with prisma and login with credentials

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import Navbar from './components/navbar/Navbar'
 import { Nunito } from 'next/font/google'
 import ClientOnly from './components/ClientOnly'
 import RegisterModal from './components/Modals/RegisterModal'
+import LoginModal from './components/Modals/LoginModal'
 import ToasterProvider from './providers/TotasterProvider'
+import getCurrentUser from './actions/getCurrentUser'
 
 export const metadata = {
   title: 'Airbnb',
@@ -12,18 +14,20 @@ export const metadata = {
 
 const font = Nunito({ subsets: ['latin'] })
 
-export default function RootLayout({
+export default async function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser()
   return (
     <html lang='en'>
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
           <RegisterModal />
-          <Navbar />
+          <LoginModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
